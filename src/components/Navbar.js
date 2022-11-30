67ab4f9c1413fa6b031739c2b7caa3faf20a5bc1
Edozie <edoziechukwuma@gmail.com>
2022-11-30 09:59:47 -0600

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from './images/icons8.png';
+import '../App.css'
+
+function Navbar() {
+  return (
+    <header className="nav-div">
+      <h1 className="logo">Air Pollution Index</h1>
+      <img src={logo} className="hamburger" alt="Hamburger Menu Icon" />
+    </header>
+  );
+}
+
+export default Navbar;
