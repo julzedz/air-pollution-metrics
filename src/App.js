a316e345104224ebde8bfd917d433c0d0822e305
Edozie <edoziechukwuma@gmail.com>
2022-12-02 16:34:36 -0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Detail from './components/pages/Detail';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/details' element={<Detail />} />
+      </Routes>
     </div>
   );
 }
