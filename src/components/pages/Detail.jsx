cd56942a0f2327cbd75d225a773014e08a05d82d
Edozie <edoziechukwuma@gmail.com>
2022-12-03 05:27:35 -0600

    Edit fav icon

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
+import data from '../data/data'
 import back from '../images/back-arrow.png'
+import { getPollutionData } from "../../redux/Slice/slice";
 
 function Detail() {
   const { nation } = useParams();
