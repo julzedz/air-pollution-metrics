bae5bbca513530210ed77277f7ccdefbcc9363a8
Edozie <edoziechukwuma@gmail.com>
2022-12-03 22:33:26 -0600

    Reinstall linters

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from '../redux/configureStore'
+import store from '../redux/configureStore';
 import Detail from '../components/pages/Detail';
 import Home from '../components/pages/Home';
 import '@testing-library/jest-dom';
--- src/tests/slice.test.js
+++ src/tests/slice.test.js
@@ -1,36 +1,35 @@
 import sliceReducer, { getPollutionData } from '../redux/Slice/slice';
 
 const initialState = [];
-describe("Tests Redux in project", () => {
-
-  it("Tests if it returns same item in the array", () => {
+describe('Tests Redux in project', () => {
+  it('Tests if it returns same item in the array', () => {
     expect(
-      sliceReducer(initialState, getPollutionData(["Albania"]))
-    ).not.toEqual(["Albania"]);
+      sliceReducer(initialState, getPollutionData(['Albania']))
+    ).not.toEqual(['Albania']);
   });
 
-  it("Tests if the state is initially an empty array", () => {
+  it('Tests if the state is initially an empty array', () => {
     expect(sliceReducer(initialState, [])).toEqual([]);
   });
 
-  it("Tests if it returns a 5 items array", () => {
+  it('Tests if it returns a 5 items array', () => {
     expect(
       sliceReducer(
         initialState,
         getPollutionData([
-          "Albania",
-          "Andorra",
-          "Austria",
-          "Belarus",
-          "Belgium",
-        ])
-      )
+          'Albania',
+          'Andorra',
+          'Austria',
+          'Belarus',
+          'Belgium',
+        ]),
+      ),
     ).not.toEqual([
-      "Albania",
-      "Andorra",
-      "Austria",
-      "Belarus",
-      "Belgium",
+      'Albania',
+      'Andorra',
+      'Austria',
+      'Belarus',
+      'Belgium',
     ]);
   });
 });
